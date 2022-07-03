661ce0ceac8260b83f7e332bd61ec1eaf3abcabf
Marc Plogas <marc.plogas@digitalnatives.de>
2022-07-03 15:20:29 +0200

    initial seed, it's not a fork anymore but a complete rewrite

---
+++ Stereokit.Demo.ASA/Program.cs
@@ -0,0 +1,49 @@
+using StereoKit;
+using System;
+
+namespace Stereokit.Demo.ASA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize StereoKit
+            SKSettings settings = new SKSettings
+            {
+                appName = "Stereokit.Demo.ASA",
+                assetsFolder = "Assets",
+                blendPreference = DisplayBlend.AnyTransparent, // we're doing this demo for HL
+                displayPreference = DisplayMode.MixedReality,
+                logFilter = LogLevel.Diagnostic //I have no idea what I'm doing so please show me everything :D
+            };
+            if (!SK.Initialize(settings))
+                Environment.Exit(1);
+
+
+
+
+
+            // Create assets used by the app
+            Pose cubePose = new Pose(0, 0, -0.5f, Quat.Identity);
+            Model cube = Model.FromMesh(
+                Mesh.GenerateRoundedCube(Vec3.One * 0.1f, 0.02f),
+                Default.MaterialUI);
+
+            Matrix floorTransform = Matrix.TS(0, -1.5f, 0, new Vec3(30, 0.1f, 30));
+            Material floorMaterial = new Material(Shader.FromFile("floor.hlsl"));
+            floorMaterial.Transparency = Transparency.Blend;
+
+
+            // Core application loop
+            while (SK.Step(() =>
+            {
+                if (SK.System.displayType == Display.Opaque)
+                    Default.MeshCube.Draw(floorMaterial, floorTransform);
+
+                UI.Handle("Cube", ref cubePose, cube.Bounds);
+                cube.Draw(cubePose.ToMatrix());
+            })) ;
+            SK.Shutdown();
+        }
+    }
+}
---
+++ Stereokit.Demo.ASA/Services/SpatialAnchorsWrapper.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.SpatialAnchors;
+using Windows.Perception.Spatial;
+using StereoKit;
+
+namespace Stereokit.Demo.ASA.Services
+{
+    internal class SpatialAnchorsWrapper
+    {
+        private readonly CloudSpatialAnchorSession cloudSession;
+        private CloudSpatialAnchorWatcher cloudSpatialAnchorWatcher;
+
+        public SpatialAnchorsWrapper(string accountId, string accountKey, string domain)
+        {
+            this.cloudSession = new CloudSpatialAnchorSession();
+            this.cloudSession.Configuration.AccountId = accountId;
+            this.cloudSession.Configuration.AccountKey = accountKey;
+            this.cloudSession.Configuration.AccountDomain = domain;
+
+            this.cloudSession.LogLevel = SessionLogLevel.All; //show me what you got!
+            //TODO: provide external event handlers
+            this.cloudSession.OnLogDebug += CloudSessionOnOnLogDebug;
+            this.cloudSession.Error += CloudSessionOnError;
+            this.cloudSession.AnchorLocated += CloudSessionOnAnchorLocated;
+            this.cloudSession.LocateAnchorsCompleted += CloudSessionOnLocateAnchorsCompleted;
+            this.cloudSession.SessionUpdated += CloudSessionOnSessionUpdated;
+
+            this.cloudSession.LocationProvider = BuildCoarseLocationProvider();
+        }
+        public void StartSession()
+        {
+            if (!HasActiveSession())
+            {
+                this.cloudSession.Start();
+            }
+        }
+
+        public void EndSession()
+        {
+            if (HasActiveSession())
+            {
+                StopLocatingAnchors();
+                this.cloudSession.Stop();
+            }
+        }
+
+        public void StartLocatingAnchors(int maxResults = 5, float distance = 10)
+        {
+            StopLocatingAnchors(); //just one watchersession is currently supported TODO: confirm
+
+            var deviceCriteria = new NearDeviceCriteria
+            {
+                DistanceInMeters = distance,
+                MaxResultCount = maxResults
+            };
+
+            var criteria = new AnchorLocateCriteria
+            {
+                NearDevice = deviceCriteria
+            };
+            this.cloudSession.CreateWatcher(criteria);
+        }
+
+        public void StartLocatingAnchors(string[] anchorIds)
+        {
+            StopLocatingAnchors(); //just one watchersession is currently supported TODO: confirm
+
+            // criteria in detail: https://docs.microsoft.com/en-us/azure/spatial-anchors/concepts/anchor-locate-strategy
+            var criteria = new AnchorLocateCriteria
+            {
+                Identifiers = anchorIds
+            };
+            this.cloudSession.CreateWatcher(criteria);
+        }
+
+        public void StopLocatingAnchors()
+        {
+            var activeWatchers = this.cloudSession.GetActiveWatchers();
+            foreach (var watcher in activeWatchers)
+            {
+                watcher.Stop();
+            }
+        }
+
+        public void CreateCloudAnchor(Pose pose)
+        {
+            var locator = SpatialLocator.GetDefault();
+            var reference = locator.CreateStationaryFrameOfReferenceAtCurrentLocation();
+            var anchor = SpatialAnchor.TryCreateRelativeTo(reference.CoordinateSystem);
+            var cloudAnchor = new CloudSpatialAnchor
+            {
+                LocalAnchor = anchor
+            };
+
+
+
+        }
+
+        private PlatformLocationProvider BuildCoarseLocationProvider()
+        {
+            //https://docs.microsoft.com/en-us/azure/spatial-anchors/concepts/coarse-reloc#platform-availability
+            //if your lens has been paired with an external GPS, hook to the UpdateSensorFingerprintRequired event and submit geolocation
+            return new PlatformLocationProvider
+            {
+                Sensors =
+                {
+                    BluetoothEnabled = true,
+                    WifiEnabled = true
+                }
+            };
+        }
+
+
+        
+        private bool HasActiveSession()
+        {
+            return !string.IsNullOrEmpty(this.cloudSession.SessionId);
+        }
+
+        private void CloudSessionOnSessionUpdated(object sender, SessionUpdatedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CloudSessionOnLocateAnchorsCompleted(object sender, LocateAnchorsCompletedEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CloudSessionOnAnchorLocated(object sender, AnchorLocatedEventArgs args)
+        {
+            //https://stereokit.net/Pages/Reference/World/FromPerceptionAnchor.html
+
+            throw new NotImplementedException();
+        }
+
+        private void CloudSessionOnError(object sender, SessionErrorEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CloudSessionOnOnLogDebug(object sender, OnLogDebugEventArgs args)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
